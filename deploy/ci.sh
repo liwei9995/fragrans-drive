0182e3a68dea73902265116970d38c6884d8b0b7
Alex.Li <liwei9995@126.com>
2023-03-29 00:01:02 +0800

    chore(deploy): add ci and cd scripts

---
+++ deploy/ci.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# 定义变量
+REGISTRY_NAME="docker.oyiyio.com"
+CONTAINER_NAME="yi-drive"
+COMMIT_SHA=$(git rev-parse --short HEAD)
+
+# 构建 yi-drive 镜像
+docker build --pull --no-cache -t $REGISTRY_NAME/$CONTAINER_NAME:$COMMIT_SHA .
+
+# 登录 Docker Registry
+echo $DOCKER_REGISTRY_PASSWORD | docker login $REGISTRY_NAME -u $DOCKER_REGISTRY_USER --password-stdin
+
+# 推送 yi-drive 到镜像仓库
+docker push $REGISTRY_NAME/$CONTAINER_NAME:$COMMIT_SHA
+
+# 登出 Docker Registry
+docker logout
