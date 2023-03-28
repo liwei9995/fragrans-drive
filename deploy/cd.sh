f76fd09765de427feb6e44c1f4812581fd907fdf
Alex.Li <liwei9995@126.com>
2023-03-29 00:21:51 +0800

    fix(deploy): fix ci/cd scripts issue

--- deploy/cd.sh
+++ deploy/cd.sh
@@ -5,13 +5,12 @@ REGISTRY_NAME="docker.oyiyio.com"
 CONTAINER_NAME="yi-drive"
 CONTAINER_PORT=8060
 CONTAINER_INNER_PORT=80
-TAG_RELEASE="release"
 
 # 登录 Docker Registry
 echo $DOCKER_REGISTRY_PASSWORD | docker login $REGISTRY_NAME -u $DOCKER_REGISTRY_USER --password-stdin
 
 # 从镜像仓库再次拉取 yi-drive 镜像
-docker pull $REGISTRY_NAME/$CONTAINER_NAME:$TAG_RELEASE
+docker pull $REGISTRY_NAME/$CONTAINER_NAME
 
 # 登出 Docker Registry
 docker logout
--- deploy/ci.sh
+++ deploy/ci.sh
@@ -4,16 +4,17 @@
 REGISTRY_NAME="docker.oyiyio.com"
 CONTAINER_NAME="yi-drive"
 COMMIT_SHA=$(git rev-parse --short HEAD)
-TAG_RELEASE="release"
+TAG_LATEST="latest"
 
 # 构建 yi-drive 镜像
-docker build --pull --no-cache -t $REGISTRY_NAME/$CONTAINER_NAME:$COMMIT_SHA -t $REGISTRY_NAME/$CONTAINER_NAME:$TAG_RELEASE .
+docker build --pull --no-cache -t $REGISTRY_NAME/$CONTAINER_NAME:$COMMIT_SHA -t $REGISTRY_NAME/$CONTAINER_NAME:$TAG_LATEST .
 
 # 登录 Docker Registry
 echo $DOCKER_REGISTRY_PASSWORD | docker login $REGISTRY_NAME -u $DOCKER_REGISTRY_USER --password-stdin
 
 # 推送 yi-drive 到镜像仓库
 docker push $REGISTRY_NAME/$CONTAINER_NAME:$COMMIT_SHA
+docker push $REGISTRY_NAME/$CONTAINER_NAME:$TAG_LATEST
 
 # 登出 Docker Registry
 docker logout
